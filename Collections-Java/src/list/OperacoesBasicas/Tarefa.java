9359e69f086807d5b6cb91677b8db85d005f575a
SuelyBrito <suelybbarbosa@hotmail.com>
2023-09-11 14:06:29 -0300

    Resolvendo desafios ListaTarefa

---
+++ Collections-Java/src/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package list.OperacoesBasicas;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Tarefa {
+    //atributos
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+    
+    
+}
