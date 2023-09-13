c01e1973479ee0b06c4e2bfe31523145d6dad5bd
SuelyBrito <suelybbarbosa@hotmail.com>
2023-09-13 10:00:21 -0300

    Resolvendo exercicios AgendaEventos,EstoqueProdutos

---
+++ Collections-Java/src/map/Ordenacao/Evento.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package map.Ordenacao;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Evento {
+    //atributos
+
+    private String nome;
+    private String atracao;
+
+    public Evento(String nome, String atracao) {
+        this.nome = nome;
+        this.atracao = atracao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAtracao() {
+        return atracao;
+    }
+
+    public void setAtracao(String atracao) {
+        this.atracao = atracao;
+    }
+
+}
---
+++ Collections-Java/src/map/Pesquisa/Produto.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package map.Pesquisa;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Produto {
+    //atributos
+
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "nome=" + nome + ", preco=" + preco + ", quantidade=" + quantidade + '}';
+    }
+
+}
